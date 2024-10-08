14f7a0412802ff6b5b58af3add03a8546f12ed6a
douglas <douglasmaxton@gmail.com>
2024-10-08 15:46:21 +0100

    feat: add logic to change manifest to application/json

--- backend/property/urls.py
+++ backend/property/urls.py
@@ -14,14 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.shortcuts import render
+from .settings import BASE_DIR
 
 from .views import HomeView
 
 def render_react(request):
-    return render(request, 'index.html')
+    path_to_manifest = os.path.join(BASE_DIR, 'build/manifest.json')
+    with open(path_to_manifest) as f:
+        return render(request, 'index.html', content_type='application/json')
 
 urlpatterns = [
     path("api/", HomeView.as_view({'get': 'list'}), name="home"),
