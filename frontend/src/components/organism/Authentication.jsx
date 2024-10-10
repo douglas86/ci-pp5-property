e4ec5b2e8b3c0a901dc695f33d75504fcb3bf658
douglas <douglasmaxton@gmail.com>
2024-10-10 17:59:04 +0100

    feat: registration in django from React Form

--- frontend/src/components/organism/Authentication.jsx
+++ frontend/src/components/organism/Authentication.jsx
@@ -1,13 +1,23 @@
 import Modal from "react-bootstrap/Modal";
 import { button } from "../atom/button";
 import useAppContext from "../../hooks/useAppContext";
-import Login from "./Forms/Login";
+import Registration from "./Forms/Registration";
+import { AxiosRegister } from "../../API/AxiosInstance";
+import { useState } from "react";
 
 const Authentication = ({ show, setShow }) => {
+  const [form, setForm] = useState({});
+
   const { modal } = useAppContext();
   const { header, btn } = modal;
 
   const handleClose = () => setShow(false);
+
+  const handleSubmit = async () => {
+    await AxiosRegister.post("/dj-rest-auth/registration/", form)
+      .then((res) => console.log("res", res))
+      .catch((err) => console.log("err", err));
+  };
 
   return (
     <Modal show={show} onHide={handleClose}>
@@ -15,11 +25,11 @@ const Authentication = ({ show, setShow }) => {
         <Modal.Title>{header}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Login />
+        <Registration form={form} setForm={setForm} />
       </Modal.Body>
       <Modal.Footer>
         {button(handleClose, "Close", "secondary")}
-        {button(handleClose, btn, "warning")}
+        {button(handleSubmit, btn, "warning")}
       </Modal.Footer>
     </Modal>
   );
--- frontend/src/components/organism/Forms/Registration.jsx
+++ frontend/src/components/organism/Forms/Registration.jsx
@@ -3,7 +3,7 @@ import { button } from "../../atom/button";
 import { useEffect } from "react";
 import useAppContext from "../../../hooks/useAppContext";
 
-const Registration = () => {
+const Registration = ({ form, setForm }) => {
   const { dispatch } = useAppContext();
 
   useEffect(() => {
@@ -18,15 +18,28 @@ const Registration = () => {
       {button(handleClick, "Click here to Login?", "link")}
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label column={true}>Username</Form.Label>
-        <Form.Control type="text" placeholder="my name" autoFocus={true} />
+        <Form.Control
+          type="text"
+          placeholder="my name"
+          autoFocus={true}
+          onChange={(e) => setForm({ ...form, username: e.target.value })}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label column={true}>Password</Form.Label>
-        <Form.Control type="password" placeholder="your password" />
+        <Form.Control
+          type="password"
+          placeholder="your password"
+          onChange={(e) => setForm({ ...form, password1: e.target.value })}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlPassword1">
         <Form.Label column={true}>Re-type Password</Form.Label>
-        <Form.Control type="password" placeholder="re-type password" />
+        <Form.Control
+          type="password"
+          placeholder="re-type password"
+          onChange={(e) => setForm({ ...form, password2: e.target.value })}
+        />
       </Form.Group>
     </Form>
   );
