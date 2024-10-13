2c8299b3dc90bc2fbbbfcf69299b0c39a3d78449
douglas <douglasmaxton@gmail.com>
2024-10-13 20:43:07 +0100

    feat: refactored the Logout form

--- frontend/src/components/organism/Forms/Logout.jsx
+++ frontend/src/components/organism/Forms/Logout.jsx
@@ -1,26 +1,48 @@
-import Form from "react-bootstrap/Form";
-import useAppContext from "../../../hooks/useAppContext";
+// 3rd party
 import { useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
+
+// utils and custom hooks
+import { onSubmit } from "../../../utils";
+import useAppContext from "../../../hooks/useAppContext";
+
+// atomic design
+import { button } from "../../atom";
+
+// styling
+import styles from "../../../styles/components/organism/Forms.module.css";
 
 const Logout = () => {
   const { dispatch } = useAppContext();
 
+  const { handleSubmit } = useForm();
+
   useEffect(() => {
     // change the header of the modal
     dispatch({ type: "CHANGE HEADER", payload: "Logout Form" });
-    // change the submit button text on modal
-    dispatch({ type: "CHANGE BTN", payload: "Logout" });
-    // change the url needed for the submitted button
-    dispatch({ type: "FORM SUBMIT URL", payload: "/auth/logout/" });
   }, [dispatch]);
 
   return (
-    <Form>
+    <Form
+      onSubmit={handleSubmit((data) =>
+        onSubmit(data, "/auth/logout/", dispatch),
+      )}
+    >
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label column={true}>
           Are your sure that you want to Logout?
         </Form.Label>
       </Form.Group>
+
+      <div className={styles.btn}>
+        {button(
+          () => dispatch({ type: "CHANGE MODAL STATE", payload: false }),
+          "Cancel",
+          "secondary",
+        )}
+        {button("submit", "Logout", "danger")}
+      </div>
     </Form>
   );
 };
