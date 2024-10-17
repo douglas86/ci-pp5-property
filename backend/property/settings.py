03bb9627e3d65502e5a1ba8a9ee072250e003183
douglas <douglasmaxton@gmail.com>
2024-10-17 16:30:40 +0100

    feat: print secret key

--- backend/property/settings.py
+++ backend/property/settings.py
@@ -29,6 +29,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SETTINGS_KEY')
 
+print(SECRET_KEY)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
