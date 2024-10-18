151818d053ccab6dbf6479952e356607ced400a9
douglas <douglasmaxton@gmail.com>
2024-10-18 11:15:57 +0100

    feat: removed JWT function from rest framework list

--- backend/property/settings.py
+++ backend/property/settings.py
@@ -83,7 +83,6 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.JWTAuthenticationFromCookie',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
