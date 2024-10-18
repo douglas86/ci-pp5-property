fe18105c9745b8a8a97b6d956d476116b46c854d
douglas <douglasmaxton@gmail.com>
2024-10-18 12:34:31 +0100

    feat: deleted login from an url path

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -4,7 +4,7 @@ from .views import LogoutView, ChangePasswordView
 
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
-    path("login/", include("dj_rest_auth.urls"), name="login"),
+    path("", include("dj_rest_auth.urls"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout" ),
     path("registration/", include("dj_rest_auth.registration.urls"), name="registration"),
     path("change_password/", ChangePasswordView.as_view({'post': 'retrieve'}), name="change_password"),
