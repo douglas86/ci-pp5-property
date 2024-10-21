372bb696a8e04ff965b3558711ba03a8d31021ed
douglas <douglasmaxton@gmail.com>
2024-10-21 19:55:30 +0100

    feat: made both social icons clickable

--- frontend/src/components/organism/Footer.jsx
+++ frontend/src/components/organism/Footer.jsx
@@ -2,25 +2,38 @@ import styles from "../../styles/components/organism/Footer.module.css";
 import { labeled } from "../atom";
 import Image from "react-bootstrap/Image";
 
-import githubLogo from "../../assets/images/GitHub.png";
-import LinkedinLogo from "../../assets/images/LinkedIn Circled.png";
+import githubLogo from "../../assets/images/footer/GitHub.png";
+import LinkedinLogo from "../../assets/images/footer/LinkedIn Circled.png";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.left}>
         {labeled("London Properties")}
-        <div className={styles.images}></div>
-        <Image
-          className={styles.image}
-          src={`${LinkedinLogo}`}
-          alt="Linkedin Logo"
-        />
-        <Image
-          className={styles.image}
-          src={`${githubLogo}`}
-          alt="Github Logo"
-        />
+        <div className={styles.images}>
+          <a
+            href="https://www.linkedin.com/in/douglas-maxton-58134b170/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.image}
+              src={`${LinkedinLogo}`}
+              alt="Linkedin Logo"
+            />
+          </a>
+          <a
+            href="https://github.com/douglas86"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.image}
+              src={`${githubLogo}`}
+              alt="Github Logo"
+            />
+          </a>
+        </div>
       </div>
       <div className={styles.right}>{labeled("Text")}</div>
     </footer>
