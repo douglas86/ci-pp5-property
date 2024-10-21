53de4180fbc9d58ceb6af8a7f40de310f03c67d5
douglas <douglasmaxton@gmail.com>
2024-10-21 22:05:40 +0100

    feat: added first content to page with styling

--- frontend/src/components/pages/AboutPage.jsx
+++ frontend/src/components/pages/AboutPage.jsx
@@ -1,15 +1,26 @@
 import PageTitle from "../molecule/PageTitle";
 import DescriptiveContent from "../molecule/DescriptiveContent";
 
+import style from "../../styles/components/pages/AboutPage.module.css";
+import Image from "react-bootstrap/Image";
+import House from "../../assets/images/house.png";
+
 const AboutPage = () => {
   return (
     <div>
       <PageTitle />
-      <DescriptiveContent
-        heading="About US"
-        subTitle="This is just a little detail about the best Properties in London"
-        paragraph="Welcome to London Properties, the best website in the Property sector. Weather you are looking to buy, rent or sell. I am sure that we can find the correct property for you."
-      />
+      <div className={style.content}>
+        <div className={style.left}>
+          <DescriptiveContent
+            heading="About US"
+            subTitle="This is just a little detail about the best Properties in London"
+            paragraph="Welcome to London Properties, the best website in the Property sector. Weather you are looking to buy, rent or sell. I am sure that we can find the correct property for you."
+          />
+        </div>
+        <div className={style.right}>
+          <Image src={`${House}`} alt="House" />
+        </div>
+      </div>
     </div>
   );
 };
