0a94fe6fd71adb4cd3fbabfa57e5cac2c22356a8
douglas <douglasmaxton@gmail.com>
2024-10-21 19:49:28 +0100

    feat: added styling for the left-hand side for GitHub and LinkedIn icons

--- frontend/src/components/organism/Footer.jsx
+++ frontend/src/components/organism/Footer.jsx
@@ -1,9 +1,28 @@
 import styles from "../../styles/components/organism/Footer.module.css";
+import { labeled } from "../atom";
+import Image from "react-bootstrap/Image";
+
+import githubLogo from "../../assets/images/GitHub.png";
+import LinkedinLogo from "../../assets/images/LinkedIn Circled.png";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h1>Footer</h1>
+      <div className={styles.left}>
+        {labeled("London Properties")}
+        <div className={styles.images}></div>
+        <Image
+          className={styles.image}
+          src={`${LinkedinLogo}`}
+          alt="Linkedin Logo"
+        />
+        <Image
+          className={styles.image}
+          src={`${githubLogo}`}
+          alt="Github Logo"
+        />
+      </div>
+      <div className={styles.right}>{labeled("Text")}</div>
     </footer>
   );
 };
