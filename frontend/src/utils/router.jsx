710e5d73e55e8a9b9f90de0d3e7a3fa0ce5a04d7
douglas <douglasmaxton@gmail.com>
2024-10-22 16:11:44 +0100

    feat: create admin and user dashboard pages

--- frontend/src/utils/router.jsx
+++ frontend/src/utils/router.jsx
@@ -2,9 +2,16 @@
 import LandingPage from "../components/pages/LandingPage";
 import AboutPage from "../components/pages/AboutPage";
 import NotFound from "../components/pages/NotFound";
+import AdminDashboard from "../components/pages/AdminDashboard";
+import UsersDashboard from "../components/pages/UsersDashboard";
 
 /**
  * Helper function used to store all url paths within this application
+ * id: unique identified to map around used for React application
+ * title: what you want displayed on navbar
+ * page: component to be loaded,
+ * path: the url path for that component
+ * role: what your role is in the organization, this is also used to protect pages from certain view
  * @type {[{path: string, id: number, page: JSX.Element, heading: string},{path: string, id: number, page: JSX.Element, heading: string},{path: string, id: number, page: JSX.Element}]}
  */
 export const router = [
@@ -21,9 +28,24 @@ export const router = [
     page: <AboutPage />,
     path: "/about",
   },
-  // error and not found pages
+  // dashboards
   {
     id: 3,
+    title: "Dashboard",
+    page: <AdminDashboard />,
+    path: "/admin_dashboard",
+    role: "admin",
+  },
+  {
+    id: 4,
+    title: "Dashboard",
+    page: <UsersDashboard />,
+    path: "/user_dashboard",
+    role: "user",
+  },
+  // error and not found pages
+  {
+    id: 5,
     page: <NotFound />,
     path: "*",
   },
