4ffef9e108ca6ac1bf8627ce1df1da6c4a9366d6
douglas <douglasmaxton@gmail.com>
2024-10-23 14:38:25 +0100

    feat: protected page if not admin user

--- frontend/src/components/pages/dashboards/AdminDashboard.jsx
+++ frontend/src/components/pages/dashboards/AdminDashboard.jsx
@@ -1,8 +1,32 @@
+import useAppContext from "../../../hooks/useAppContext";
+import { useEffect } from "react";
+
 const AdminDashboard = () => {
+  const { dispatch, user, isAdmin } = useAppContext();
+
+  useEffect(() => {
+    // check if user is not admin
+    // if user is not an admin display login form
+    if (isAdmin === null) {
+      dispatch({ type: "WHICH FORM TO USE", payload: "" });
+      dispatch({ type: "CHANGE MODAL STATE", payload: true });
+      // if a user is an admin close modal
+    } else {
+      dispatch({ type: "CHANGE MODAL STATE", payload: false });
+    }
+  }, [isAdmin]);
+
+  console.log("user", user);
+  console.log("isAdmin", isAdmin);
+
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-    </div>
+    <>
+      {user ? (
+        <h1>Admin Dashboard</h1>
+      ) : (
+        <h1>This is a protected page please login</h1>
+      )}
+    </>
   );
 };
 
