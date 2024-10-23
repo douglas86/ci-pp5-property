4a0758f9869a26e23f62e41d87d0aae15f9ba17e
douglas <douglasmaxton@gmail.com>
2024-10-23 22:39:11 +0100

    feat: deleted tables and its components

--- frontend/src/components/pages/admin/Users.jsx
+++ frontend/src/components/pages/admin/Users.jsx
@@ -1,49 +1,27 @@
 // atomic design
 import Panel from "../../organism/Admin/Panel";
-import DataTable from "../../organism/Admin/DataTable";
 
 // custom hooks
 import useAdmin from "../../../hooks/useAdmin";
 import useAppContext from "../../../hooks/useAppContext";
 import useFetch from "../../../hooks/useFetch";
+import DescriptiveContent from "../../molecule/DescriptiveContent";
 
 const Users = () => {
   const { isAdmin } = useAppContext();
 
   const { data } = useFetch("profile/", isAdmin);
 
-  console.log("data", data);
-
   // custom hook for admin check
   useAdmin();
 
-  console.log("isAdmin", isAdmin);
-
-  const heading = [
-    "#",
-    "",
-    "Name of Person",
-    "Address",
-    "Area code",
-    "Role",
-    "Rent",
-  ];
-  const body = [
-    1,
-    "image",
-    "name of person",
-    "address of property",
-    "area code",
-    "user",
-    "£ 1000",
-  ];
+  console.log("data", data);
 
   return (
     <>
       {isAdmin ? (
         <>
           <Panel />
-          <DataTable heading={heading} body={data} />
         </>
       ) : (
         <h1>This is a protected page please login</h1>
