e45bbc8a287d50fa99105e64d15b23cdecf147e2
douglas <douglasmaxton@gmail.com>
2024-11-09 21:04:29 +0000

    feat: changed api endpoint in heroku url

--- frontend/src/utils/apiSettings.jsx
+++ frontend/src/utils/apiSettings.jsx
@@ -2,7 +2,7 @@
 const env = process.env.REACT_APP_NODE_ENV || "development";
 
 // heroku api
-const heroku = "https://ci-pp5-property-v2-api-cc7edcd1041d.herokuapp.com";
+const heroku = "https://ci-pp5-property-django-bcd4d87abb1f.herokuapp.com";
 // local api
 const local = "http://localhost:8000";
 
